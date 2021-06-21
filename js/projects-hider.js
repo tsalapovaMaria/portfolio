43898780268b4afb29dd7378bbf54dc92fd46f40
Maria <mazhats@yandex.ru>
2021-06-21 17:22:51 +0300

    create skill bar animation and start to create project hider

---
+++ js/projects-hider.js
@@ -0,0 +1,5 @@
+const arrow = document.querySelector('.projects-count > svg');
+
+arrow.addEventListener('click', () => {
+    arrow.classList.toggle('open');
+})
---
+++ js/skills-bar-animation.js
@@ -0,0 +1,18 @@
+const skills = document.querySelectorAll('.skills');
+const maxProgressValue = document.querySelector('.skills-bar').offsetWidth;
+
+const skillProgress = document.createElement('span');
+skillProgress.className = 'skill-progress-tooltip';
+
+Array.from(skills).forEach(skill => {
+    skill.addEventListener('mouseover', (event) => {
+        skillProgress.textContent = Math.round((skill.offsetWidth / maxProgressValue) * 100) + ' %';
+
+        skill.append(skillProgress);
+        skillProgress.style.top = skill.offsetTop  - skillProgress.offsetTop + 'px';
+        skillProgress.style.left = skill.offsetWidth / 2 - skillProgress.offsetWidth / 2 + 'px';
+    });
+    skill.addEventListener('mouseout', () => {
+        skillProgress.remove();
+    });
+});
