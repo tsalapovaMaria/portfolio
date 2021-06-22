68422028a8502b69e212c10f6df557db013e3271
Maria <mazhats@yandex.ru>
2021-06-22 12:38:14 +0300

    create custom disabled checkbox and create event on projects count container

--- js/projects-hider.js
+++ js/projects-hider.js
@@ -1,8 +1,9 @@
+const panelHider = document.querySelector('.projects-count');
 const arrow = document.querySelector('.projects-count > svg');
 const projects = document.querySelectorAll('.project');
 
 
-arrow.addEventListener('click', () => {
+panelHider.addEventListener('click', () => {
     arrow.classList.toggle('open');
     Array.from(projects).forEach(project => {
         project.style.visibility = (project.style.visibility === 'hidden')? 'visible' : 'hidden';
